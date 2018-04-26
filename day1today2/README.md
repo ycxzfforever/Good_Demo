88572386ebcf0fdd5a21783aee7c5f53519b5729
ycxzfforever <ycxzfforever@163.com>
2018-04-26 09:24:07 +0800

    计算两个日期之间的间隔天数，比较经典的算法

---
+++ day1today2/README.md
@@ -0,0 +1,2 @@
+[C语言计算日期间隔天数的经典算法解析](https://www.linuxidc.com/Linux/2015-02/114053.htm)<br>
+[两个日期差值](https://blog.csdn.net/xq4664546/article/details/51751015)
---
+++ day1today2/main.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+
+int Day1ToDay2(int year_start, int month_start, int day_start
+             , int year_end, int month_end, int day_end)
+{
+    int y2, m2, d2;
+    int y1, m1, d1;
+
+    m1 = (month_start + 9) % 12;
+    y1 = year_start - m1/10;
+    d1 = 365*y1 + y1/4 - y1/100 + y1/400 + (m1*306 + 5)/10 + (day_start - 1);	
+	printf("m1=%d y1=%d d1=%d t1=%d t2=%d\n",m1,y1,d1,y1/4 - y1/100 + y1/400,(m1*306 + 5)/10);
+
+    m2 = (month_end + 9) % 12;
+    y2 = year_end - m2/10;
+    d2 = 365*y2 + y2/4 - y2/100 + y2/400 + (m2*306 + 5)/10 + (day_end - 1);
+	printf("m2=%d y2=%d d2=%d\n",m2,y2,d2);
+    return (d2 - d1);
+}
+
+int main()
+{
+    int ret;
+    ret=Day1ToDay2(2015,10,16,2015,12,30);
+    printf("ret=%d\n",ret);
+    getchar();
+    return 0;
+}
